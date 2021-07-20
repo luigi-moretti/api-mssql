c22ee1983df8a26a2a771ee92da07c42e2a79146
luigi-moretti <66831232+luigi-moretti@users.noreply.github.com>
2021-07-20 19:53:35 -0300

    melhoria procedures

--- dados/criar-tabelas-mssql.sql
+++ dados/criar-tabelas-mssql.sql
@@ -81,3 +81,78 @@ CREATE OR ALTER PROCEDURE consultaCTEPorChave
 @chaves TABLE
 AS
 select * from CTE where CHAVECTE in @chaves
+
+
+CREATE OR ALTER PROCEDURE consultaCTEPorChave
+@lista NVARCHAR(MAX) = null,
+@status NVARCHAR(MAX) = null
+AS
+DECLARE @pos INT
+DECLARE @nextpos INT
+DECLARE @valuelen INT
+DECLARE @tbl TABLE (number NVARCHAR(MAX))
+
+
+DECLARE @Spos INT
+DECLARE @Snextpos INT
+DECLARE @Svaluelen INT
+DECLARE @Stbl TABLE (number NVARCHAR(MAX))
+SELECT @pos = 0, @nextpos = 1;
+
+--IF @lista IS NOT NULL
+WHILE @nextpos > 0
+BEGIN
+    SELECT @nextpos = charindex(',', @lista, @pos + 1)
+    SELECT @valuelen = CASE WHEN @nextpos > 0
+                            THEN @nextpos
+                            ELSE len(@lista) + 1
+                        END - @pos - 1
+    INSERT @tbl (number)
+        VALUES (convert(NVARCHAR(MAX), substring(@lista, @pos + 1, @valuelen)))
+    SELECT @pos = @nextpos;
+END
+
+
+SELECT @Spos = 0, @Snextpos = 1;
+	WHILE @Snextpos > 0
+	BEGIN
+		SELECT @Snextpos = charindex(',', @status, @Spos + 1)
+		SELECT @Svaluelen = CASE WHEN @Snextpos > 0
+								THEN @Snextpos
+								ELSE len(@status) + 1
+							END - @Spos - 1
+		INSERT @Stbl (number)
+			VALUES (convert(NVARCHAR(MAX), substring(@status, @Spos + 1, @Svaluelen)))
+		SELECT @Spos = @Snextpos;
+	END
+select CTE.*
+from CTE 
+where 1=1
+and (CASE WHEN @lista is NULL then 'A' else CTE.CHAVECTE end) in (select case when number is null then 'A' else number end from @tbl)
+and (CASE when @status is NULL then 'A' else CTE.STATUS end) in (select  case when number is null then 'A' else number end from @Stbl)
+;
+EXEC consultaCTEPorChave  @status='CANCELADO,AUTORIZADO', @lista='33659708046795850800570010013155058314981119,33659708046795850800570010013155058314986026,33659708046795850800570010013155058314986026,33659708046795850800570010013154358314942415'
+;
+
+CREATE OR ALTER PROCEDURE consultaMDFEPorChave
+@lista NVARCHAR(MAX)
+AS
+DECLARE @pos INT
+DECLARE @nextpos INT
+DECLARE @valuelen INT
+DECLARE @tbl TABLE (number NVARCHAR(MAX) NOT NULL)
+
+SELECT @pos = 0, @nextpos = 1;
+
+WHILE @nextpos > 0
+BEGIN
+    SELECT @nextpos = charindex(',', @lista, @pos + 1)
+    SELECT @valuelen = CASE WHEN @nextpos > 0
+                            THEN @nextpos
+                            ELSE len(@lista) + 1
+                        END - @pos - 1
+    INSERT @tbl (number)
+        VALUES (convert(NVARCHAR(MAX), substring(@lista, @pos + 1, @valuelen)))
+    SELECT @pos = @nextpos;
+END
+select * from MDFE where CHAVEMDFE in(select * from @tbl)
